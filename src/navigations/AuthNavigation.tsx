5810f340576eb6fbac4bedee8a3145f3bbb66c6e
Mustafa AĞRAŞ <131910712+mustafagras@users.noreply.github.com>
2023-12-28 15:29:46 +0300

    navigation options

--- src/navigations/AuthNavigation.tsx
+++ src/navigations/AuthNavigation.tsx
@@ -4,20 +4,32 @@ import LoginContainer from "../pages/login";
 import RegisterContainer from "../pages/register";
 import ForgotPasswordContainer from "../pages/forgotPassword";
 import { navigationPage } from "../constants/navigationConstant";
+import tr from "../JSON/tr";
 
 const Stack = createStackNavigator();
+type Customtypr = {
+  name: string;
+};
 
 export default function AuthNavigation() {
+  const { LOGIN, REGISTER, FORGOT_PASSWORD } = navigationPage;
+  const options = (navigationPage) => tr[0].auth.signIn[navigationPage];
   return (
-    <Stack.Navigator initialRouteName={navigationPage.LOGIN}>
-      <Stack.Screen name={navigationPage.LOGIN} component={LoginContainer} />
+    <Stack.Navigator initialRouteName={LOGIN}>
       <Stack.Screen
-        name={navigationPage.REGISTER}
-        component={RegisterContainer}
+        name={LOGIN}
+        component={LoginContainer}
+        options={{ title: `${options(LOGIN)}` }}
       />
       <Stack.Screen
-        name={navigationPage.FORGOT_PASSWORD}
+        name={REGISTER}
+        component={RegisterContainer}
+        options={{ title: `${options(REGISTER)}` }}
+      />
+      <Stack.Screen
+        name={FORGOT_PASSWORD}
         component={ForgotPasswordContainer}
+        options={{ title: `${options(FORGOT_PASSWORD)}` }}
       />
     </Stack.Navigator>
   );
