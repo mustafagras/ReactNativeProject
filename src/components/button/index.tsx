8e5d3dc58958817fbc2cdd11fa3b4170cd2cb5fa
Mustafa AĞRAŞ <131910712+mustafagras@users.noreply.github.com>
2023-12-29 09:39:49 +0300

    updated customText and custom Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
-import {
-  CustomText,
-  ForgotPasswordText,
-  SignInText,
-  SignUpText,
-} from "../text";
 import { CustomButtonProps } from "../../types";
+import CustomText from "../text";
 import styles from "./buttonStyle";
 
-const CustomButton = ({ tx, color, onPress }: CustomButtonProps) => {
+export default function CustomButton({
+  tx,
+  color,
+  onPress,
+}: CustomButtonProps) {
   return (
     <TouchableOpacity
       style={[styles.button, color && { backgroundColor: color }]}
@@ -18,39 +17,4 @@ const CustomButton = ({ tx, color, onPress }: CustomButtonProps) => {
       <CustomText tx={tx} style={styles.buttonText} />
     </TouchableOpacity>
   );
-};
-
-const SignInButton = ({ tx, color, onPress }: CustomButtonProps) => {
-  return (
-    <TouchableOpacity
-      style={[styles.button, color && { backgroundColor: color }]}
-      onPress={onPress}
-    >
-      <SignInText tx={tx} style={styles.buttonText} />
-    </TouchableOpacity>
-  );
-};
-
-const SignUpButton = ({ tx, color, onPress }: CustomButtonProps) => {
-  return (
-    <TouchableOpacity
-      style={[styles.button, color && { backgroundColor: color }]}
-      onPress={onPress}
-    >
-      <SignUpText tx={tx} style={styles.buttonText} />
-    </TouchableOpacity>
-  );
-};
-
-const ForgotPasswordButton = ({ tx, color, onPress }: CustomButtonProps) => {
-  return (
-    <TouchableOpacity
-      style={[styles.button, color && { backgroundColor: color }]}
-      onPress={onPress}
-    >
-      <ForgotPasswordText tx={tx} style={styles.buttonText} />
-    </TouchableOpacity>
-  );
-};
-
-export { CustomButton, SignInButton, SignUpButton, ForgotPasswordButton };
+}
--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -3,29 +3,21 @@ import { Text } from "react-native";
 import { CustomTextProps } from "../../types";
 import tr from "../../JSON/tr";
 
-const CustomText = ({ tx, style }: CustomTextProps) => {
-  const customText = tr[tx];
+export default function CustomText({ tx, style }: CustomTextProps) {
+  const deepLookup = (obj, path) => {
+    const keys = path.split(":");
+    let result = obj;
+
+    for (const key of keys) {
+      result = result?.[key];
+      if (result === undefined) {
+        break;
+      }
+    }
+
+    return result;
+  };
+
+  const customText = deepLookup(tr[0], tx) || tx;
   return <Text style={style}>{customText}</Text>;
-};
-
-const AuthText = ({ tx, style }: CustomTextProps) => {
-  const authText = tr[0].auth[tx];
-  return <Text style={style}>{authText}</Text>;
-};
-
-const SignInText = ({ tx, style }: CustomTextProps) => {
-  const signInText = tr[0].auth.signIn[tx];
-  return <Text style={style}>{signInText}</Text>;
-};
-
-const SignUpText = ({ tx, style }: CustomTextProps) => {
-  const signUpText = tr[0].auth.signUp[tx];
-  return <Text style={style}>{signUpText}</Text>;
-};
-
-const ForgotPasswordText = ({ tx, style }: CustomTextProps) => {
-  const forgotPasswordText = tr[0].auth.forgotPassword[tx];
-  return <Text style={style}>{forgotPasswordText}</Text>;
-};
-
-export { CustomText, AuthText, SignInText, SignUpText, ForgotPasswordText };
+}
