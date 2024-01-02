3fd5fc6954b4b3769f27a9abb530fedc9a0ace51
Mustafa AĞRAŞ <131910712+mustafagras@users.noreply.github.com>
2024-01-02 15:06:03 +0300

    add tabsicons

--- src/navigations/AppNavigation.tsx
+++ src/navigations/AppNavigation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { navigationPage } from "../constants/navigationConstant";
 import DashboardContainer from "../pages/dashboard";
@@ -10,8 +11,63 @@ const Tabs = createBottomTabNavigator();
 
 export default function AppNavigation() {
   const { HOME, EDUCATION_LIST, EVENT_LIST, PROFILE } = navigationPage;
+
+  const ImageRender = ({ uri }) => {
+    return <Image style={{ width: 20, height: 20 }} source={uri} />;
+  };
+
+  const tabBarFunc = (route, focused) => {
+    switch (route.name) {
+      case HOME:
+        return (
+          <ImageRender
+            uri={
+              focused
+                ? require("../../assets/tabIcon/homeActive.png")
+                : require("../../assets/tabIcon/home.png")
+            }
+          />
+        );
+      case EDUCATION_LIST:
+        return (
+          <ImageRender
+            uri={
+              focused
+                ? require("../../assets/tabIcon/educationActive.png")
+                : require("../../assets/tabIcon/education.png")
+            }
+          />
+        );
+      case EVENT_LIST:
+        return (
+          <ImageRender
+            uri={
+              focused
+                ? require("../../assets/tabIcon/eventActive.png")
+                : require("../../assets/tabIcon/event.png")
+            }
+          />
+        );
+      case PROFILE:
+        return (
+          <ImageRender
+            uri={
+              focused
+                ? require("../../assets/tabIcon/userActive.png")
+                : require("../../assets/tabIcon/user.png")
+            }
+          />
+        );
+    }
+  };
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          return tabBarFunc(route, focused);
+        },
+      })}
+    >
       <Tabs.Screen name={HOME} component={DashboardContainer} />
       <Tabs.Screen name={EDUCATION_LIST} component={EducationListContainer} />
       <Tabs.Screen name={EVENT_LIST} component={EventListContainer} />
