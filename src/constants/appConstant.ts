bfeb939f9d98bd6974fea6855fd62d2be4ae419d
Mustafa AĞRAŞ <131910712+mustafagras@users.noreply.github.com>
2024-01-03 09:42:10 +0300

    .

--- src/constants/appConstant.ts
+++ src/constants/appConstant.ts
@@ -1,5 +1,5 @@
 import { Platform } from "react-native";
 
-export const IS_IOS = (Platform.OS = "ios");
+export const IS_IOS = Platform.OS === "ios";
 
 export const embedGoogleURL = "http://docs.google.com/gview?embedded=true&url=";
