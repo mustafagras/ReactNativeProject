6b5bfe91de59c91dcd24d80fe5f5c21dd7f3a62b
mustafagras <mustafagras@gmail.com>
2024-01-03 19:26:28 +0300

    api req res with axios

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
-import Loader from './src/components/loader';
-import CustomSafeAreaView from './src/components/wrapper';
-import MainNavigation from './src/navigations';
+import { useEffect } from "react";
+import Loader from "./src/components/loader";
+import CustomSafeAreaView from "./src/components/wrapper";
+import MainNavigation from "./src/navigations";
+import axios from "axios";
 
 export default function App() {
+  const onGetFunc = () => {
+    axios
+      .get("https://wissenreactnative-default-rtdb.firebaseio.com/example.json")
+      .then((res) => {
+        console.log("response", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onGetFuncAsync = async () => {
+    const res = await axios.get(
+      "https://wissenreactnative-default-rtdb.firebaseio.com/exampleAsync.json"
+    );
+
+    console.log("response", res.data);
+  };
+
+  const onPostFunc = () => {
+    axios
+      .post(
+        "https://wissenreactnative-default-rtdb.firebaseio.com/example.json",
+        { name: "mustafa" }
+      )
+      .then((res) => {
+        console.log("response", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onPostFuncAsync = async () => {
+    const res = await axios.post(
+      "https://wissenreactnative-default-rtdb.firebaseio.com/exampleAsync.json",
+      { name: "mustafa" }
+    );
+
+    console.log("response", res.data);
+  };
+
+  useEffect(() => {
+    onGetFunc();
+    // onPostFunc();
+    // onPostFuncAsync();
+  }, []);
+
   return (
     <CustomSafeAreaView>
       <MainNavigation />
       <Loader />
     </CustomSafeAreaView>
   );
-};
+}
